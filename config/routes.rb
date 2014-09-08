0305c7be38fa9db1c1ef8f071d562abfe490e6aa
Gennady Kudelya <gennady.kudelya@gmail.com>
2014-09-08 14:49:48 +0300

    Added locale selector, finished localization

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :tasks
 
   devise_for :users
+  get '/english',  to: 'application#set_to_english'
+  get '/russian',  to: 'application#set_to_russian'
 
   root 'tasks#index'
 
